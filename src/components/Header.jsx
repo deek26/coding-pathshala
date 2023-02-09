ab600b336132cdd05f315610c827a31f4305b98e
deek26 <dbhargava675@gmail.com>
2023-02-09 15:36:45 +0530

    Header appearence  completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,37 @@ import {GoSearch} from 'react-icons/go'
 import {FiUsers} from 'react-icons/fi'
 import {FiHeart} from 'react-icons/fi'
 import {BsHandbag} from 'react-icons/bs'
+import {IoIosArrowDown} from 'react-icons/io'
+
 
 export default function Header() {
+
+
+
+
+      function dropdown(){
+        <div>
+          hello
+        </div>
+
+        
+      }
+
+
   return (
+<>
 
-
-    <>
-
-<div>
+  <div>
       <header >
-          <div className='flex justify-end items-center bg-black h-[50px]  '>
+          <div className='flex justify-end items-center bg-black h-[40px]  '>
             <div >
-              <ul className='flex space-x-4 mr-[130px] cursor-pointer'>
-                <li className='text-white font-semibold '>My Account</li>
-                <li className='bg-white w-[2px]'></li>
-                <li className='text-white font-semibold '>FAQs</li>
-                <li className='bg-white w-[2px]'></li>
-                <li className='text-white font-semibold flex justify-center items-center '>Contact Us
-                 <ImWhatsapp className='ml-3 h-7 w-7'/>
+              <ul className='flex space-x-4 mr-[130px]'>
+                <li className='text-white font-semibold text-[15px] cursor-pointer'>My Account</li>
+                <li className='bg-white w-[1px] h-3 mt-[6px]'></li>
+                <li className='text-white font-semibold text-[15px] cursor-pointer'>FAQs</li>
+                <li className='bg-white w-[1px] h-3 mt-[6px]'></li>
+                <li className='text-white font-semibold text-[15px]  cursor-pointer flex justify-center items-center '>Contact Us
+                 <ImWhatsapp className='ml-3 h-5 w-5'/>
                 </li>    
               </ul>
              </div>
@@ -30,60 +43,86 @@ export default function Header() {
           </div>
 
 
-
-
           <div className='flex '>
+             <div>
+                <img src={logo} alt='logo' className='h-[150px] w-[200px] ' />
+               </div>
 
-
-                <div className='h-[100px] w-[200px] '>
-                  <img src={logo} alt='logo' />
-                </div>
-
-
-                <div className='flex items-center'>
-
-                      <div className='flex items-center'>
+               <div className='flex items-center space-x-5'>
+                  <div className='flex items-center relative'>
                         <input type='search' 
                         placeholder='Search entire store here....'
                         className='border-[2px] border-black w-[500px] h-[55px] rounded ml-48 pl-4'/>
-                        <GoSearch className=' h-[55px] w-[60px] bg-blue-300 p-[13px] rounded' />
-                      </div>
+                        <GoSearch className=' h-[55px] w-[60px] bg-blue-300 p-[13px] rounded absolute right-0' />
+                  </div>
 
-                      <div className='flex items-center'> 
-                        <FiUsers className='h-[30px] w-[40px]' />
+                  <div className='flex items-center'> 
+                      <FiUsers className='h-[30px] w-[40px]' />
                     
-                          <div className='text-md'>
-                            <p>Sign In</p>
-                            <p>Create an Account</p>
-                          </div>
-                      
-                        
-                      </div>
+                  <div className='text-md cursor-pointer'>
+                     <p>Sign In</p>
+                     <p>Create an Account</p>
+                  </div>
+                </div>
 
-                      <div className='flex items-center'> 
-                        <FiHeart className='h-[30px] w-[40px]' />
+                  <div className='flex items-center'> 
+                     <FiHeart className='h-[30px] w-[40px] cursor-pointer' />
                     
-                          <div className='text-md'>
+                          <div className='text-md cursor-pointer'>
                             <p>Favorite</p>
                             <p>My Wish List</p>
                           </div>
                       </div>
                           <div className='flex items-center'> 
-                            <BsHandbag className='h-[30px] w-[40px]' />
+                            <BsHandbag className='h-[30px] w-[40px] cursor-pointer' />
                     
-                          <div className='text-md'>
+                          <div className='text-md cursor-pointer'>
                             <p>My Cart</p>
                             <p>₹0.00</p>
                           </div>
 
-                      </div>
+                      </div>        
+                </div>  
 
-                        
-                        
-                </div>
-
-               
           </div>
+
+
+          <div className='flex text-white space-x-[30px] items-center bg-black h-[60px] font-semibold cursor-pointer'>
+
+                    <div className='flex space-x-8 ml-[100px] border-[#6b705c] border-r-[1px] ' onClick={()=>dropdown}>
+                        <p className='text-white'>
+                          ALL CATEGORIES
+                        </p>
+
+                        <div className='flex flex-col space-y-1 justify-center pr-[100px]'>
+                          <div className=' w-7 h-[3px] bg-white'></div>
+                          <div className=' w-7 h-[4px] bg-white'></div>
+                          <div className=' w-7 h-[3px] bg-white'></div>
+                        </div>
+
+
+                    </div>
+                    <div className='flex space-x-2 items-center cursor-pointer'>
+                      <p>Our Products</p>
+                      <IoIosArrowDown/>
+                    </div>
+                    <div className='flex space-x-2 items-center cursor-pointer'>
+                      <p>Grow With Us</p>
+                      <IoIosArrowDown/>
+                    </div>
+                    <div className='cursor-pointer'>
+                      <p>Coaching Institues</p>
+                    </div>
+                    <div className='cursor-pointer'>
+                      <p>Media Coverage</p>
+                    </div>
+                    <div className='flex space-x-2 items-center cursor-pointer'>
+                      <p>Free Content</p>
+                      <IoIosArrowDown/>
+                    </div>
+
+          </div>
+
            
       </header>
 
